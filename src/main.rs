dab2ee1ebcfda64f2cec0ed9e582862946cec78c
dominik-korsa <29484605+dominik-korsa@users.noreply.github.com>
2025-07-06 23:29:54 +0200

    Process sceneries in parallel

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,50 @@
+use rayon::iter::ParallelIterator;
 mod parse;
 mod svg;
 mod math;
 
 use crate::parse::parse;
 use crate::svg::create_svg;
+use anyhow::bail;
+use indicatif::ParallelProgressIterator;
+use rayon::iter::IntoParallelRefIterator;
 use std::fs;
 use std::fs::File;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
+
+fn process_scenery(dir: &Path) -> anyhow::Result<()> {
+    let Some(filename) = dir.file_name() else {
+        bail!("Failed to get file name from path: {}", dir.display());
+    };
+    let name = filename.to_str().ok_or_else(|| {
+        anyhow::anyhow!("Failed to convert file name to string: {}", filename.to_string_lossy())
+    })?;
+    let file = File::open(dir.join( format!("{name}.sc")))?;
+    let parse_result = parse(file)?;
+    let output_path = PathBuf::from(format!("output/{name}.svg"));
+    create_svg(&parse_result, &output_path)?;
+    // println!("Processed scenery: {}", name);
+    Ok(())
+}
 
 fn main() {
     let input_dir = "/home/dkgl/Documents/TTSK/TrainDriver2/SavedStations";
     fs::create_dir_all("output").unwrap();
-    for file in fs::read_dir(input_dir).unwrap() {
-        let file = file.unwrap();
-        if file.file_type().ok().is_some_and(|x| x.is_dir()) {
-            let name = file.file_name().to_str().unwrap().to_string();
-            println!("Processing directory: {}", name);
-            let file = File::open(file.path().join( format!("{name}.sc"))).unwrap();
-            let parse_result = parse(file).unwrap();
-            let output_path = PathBuf::from(format!("output/{name}.svg"));
-            create_svg(&parse_result, &output_path).unwrap();
-        }
-    }
+    let directories: Vec<PathBuf> =  fs::read_dir(input_dir).unwrap()
+        .filter_map(|entry| {
+            let entry = entry.ok()?;
+            entry
+                .file_type()
+                .ok()
+                .is_some_and(|x| x.is_dir())
+                .then(|| entry.path())
+        })
+        .collect();
+    println!("Found {} scenery candidates", directories.len());
+    directories
+        .par_iter()
+        .progress_count(directories.len() as u64)
+        .for_each(|entry| {
+            process_scenery(&entry).unwrap();
+        });
 }
